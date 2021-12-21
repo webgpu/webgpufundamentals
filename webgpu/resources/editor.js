da76c352a89dd80266d55afb65ef57f7a57deab4
greggman <github@greggman.com>
2021-12-21 03:10:16 +0000

    Deploying to gh-pages from  @ c4c6195149d9f4fa3bee4c9877579df3ccea3e7a 🚀

--- webgpu/resources/editor.js
+++ webgpu/resources/editor.js
@@ -75,7 +75,18 @@ function fixCSSLinks(url, source) {
 /** @type {Globals} */
 const g = {
   html: '',
+  visible: false,
 };
+
+{
+  const onChange = function(entries) {
+    for (const entry of entries) {
+      g.visible = entry.isIntersecting;
+    }
+  };
+  const observer = new IntersectionObserver(onChange, {});
+  observer.observe(document.body);
+}
 
 /**
  * This is what's in the sources array
@@ -234,7 +245,7 @@ async function parseHTML(url, html) {
   const inlineScriptRE = /<script>([^]*?)<\/script>/i;
   const inlineModuleScriptRE = /<script type="module">([^]*?)<\/script>/i;
   const externalScriptRE = /(<!--(?:(?!-->)[\s\S])*?-->\n){0,1}<script\s+(type="module"\s+)?src\s*=\s*"(.*?)"\s*>\s*<\/script>/ig;
-  const dataScriptRE = /(<!--(?:(?!-->)[\s\S])*?-->\n){0,1}<script(.*?src=".*?)>([^]*?)<\/script>/ig;
+  const dataScriptRE = /(<!--(?:(?!-->)[\s\S])*?-->\n){0,1}<script(.*?id=".*?)>([^]*?)<\/script>/ig;
   const cssLinkRE = /<link ([^>]+?)>/g;
   const isCSSLinkRE = /type="text\/css"|rel="stylesheet"/;
   const hrefRE = /href="([^"]+)"/;
@@ -362,8 +373,8 @@ function makeBlobURLsForSources(scriptInfo) {
       scriptInfo.numLinesBeforeScript = 0;
       if (scriptInfo.isWorker) {
         const extra = `self.lessonSettings = ${JSON.stringify(lessonSettings)};
-importScripts('${dirname(scriptInfo.fqURL)}/resources/webgl-debug-helper.js');
-importScripts('${dirname(scriptInfo.fqURL)}/resources/lessons-worker-helper.js')`;
+import '${dirname(scriptInfo.fqURL)}/resources/webgl-debug-helper.js';
+import '${dirname(scriptInfo.fqURL)}/resources/lessons-worker-helper.js';`;
         scriptInfo.numLinesBeforeScript = extra.split('\n').length;
         text = `${extra}\n${text}`;
       }
@@ -478,27 +489,32 @@ function makeScriptsForWorkers(scriptInfo) {
   }
 
   const scripts = makeScriptsForWorkersImpl(scriptInfo);
-  const mainScript = scripts.pop().text;
-  if (!scripts.length) {
+  if (scripts.length === 1) {
     return {
-      js: mainScript,
+      js: scripts[0].text,
       html: '',
     };
   }
 
-  const workerName = scripts[scripts.length - 1].name;
+  // scripts[last]      = main script
+  // scripts[last - 1]  = worker
+  const mainScriptInfo = scripts[scripts.length - 1];
+  const workerScriptInfo = scripts[scripts.length - 2];
+  const workerName = workerScriptInfo.name;
+  mainScriptInfo.text = mainScriptInfo.text.split(`'${workerName}'`).join('getWorkerBlob()');
   const html = scripts.map((nameText) => {
     const {name, text} = nameText;
-    return `<script id="${name}" type="x-worker">\n${text}\n</script>`;
+    return `<script id="${name}" type="x-worker">\n${text}\n</script>\n`;
   }).join('\n');
   const init = `
 
 
 
 // ------
-// Creates Blobs for the Worker Scripts so things can be self contained for snippets/JSFiddle/Codepen
+// Creates Blobs for the Scripts so things can be self contained for snippets/JSFiddle/Codepen
+// even though they are using workers
 //
-function getWorkerBlob() {
+(function() {
   const idsToUrls = [];
   const scriptElements = [...document.querySelectorAll('script[type=x-worker]')];
   for (const scriptElement of scriptElements) {
@@ -511,11 +527,14 @@ function getWorkerBlob() {
     const id = scriptElement.id;
     idsToUrls.push({id, url});
   }
-  return idsToUrls.pop().url;
-}
+  window.getWorkerBlob = function() {
+    return idsToUrls.pop().url;
+  };
+  import(window.getWorkerBlob());
+}());
 `;
   return {
-    js: mainScript.split(`'${workerName}'`).join('getWorkerBlob()') + init,
+    js: init,
     html,
   };
 }
@@ -586,6 +605,141 @@ function openInJSFiddle() {
   window.frameElement.ownerDocument.body.removeChild(elem);
 }
 
+function openInJSGist() {
+  const comment = `// ${g.title}
+// from ${g.url}
+
+
+`;
+  getSourcesFromEditor();
+  const scripts = makeScriptsForWorkers(g.rootScriptInfo);
+  const gist = {
+    name: g.title,
+    settings: {},
+    files: [
+      { name: 'index.html', content: scripts.html + fixHTMLForCodeSite(htmlParts.html.sources[0].source), },
+      { name: 'index.css', content: htmlParts.css.sources[0].source, },
+      { name: 'index.js', content: comment + fixJSForCodeSite(scripts.js), },
+    ],
+  };
+
+  window.open('https://jsgist.org/?newGist=true', '_blank');
+  const send = (e) => {
+    e.source.postMessage({type: 'newGist', data: gist}, '*');
+  };
+  window.addEventListener('message', send, {once: true});
+}
+
+/*
+
+<!-- begin snippet: js hide: false console: true babel: false -->
+
+<!-- language: lang-js -->
+
+    console.log();
+
+<!-- language: lang-css -->
+
+    h1 { color: red; }
+
+<!-- language: lang-html -->
+
+    <h1>foo</h1>  
+
+<!-- end snippet -->
+
+*/
+
+function indent4(s) {
+  return s.split('\n').map(s => `    ${s}`).join('\n');
+}
+
+function openInStackOverflow() {
+  const comment = `// ${g.title}
+// from ${g.url}
+
+
+`;
+  getSourcesFromEditor();
+  const scripts = makeScriptsForWorkers(g.rootScriptInfo);
+  const mainHTML = scripts.html + fixHTMLForCodeSite(htmlParts.html.sources[0].source);
+  const mainJS = comment + fixJSForCodeSite(scripts.js);
+  const mainCSS = htmlParts.css.sources[0].source;
+  const asModule = /\bimport\b/.test(mainJS);
+  // Three.js wants us to use modules but Stack Overflow doesn't support them
+  const text = asModule
+    ? `
+<!-- begin snippet: js hide: false console: true babel: false -->
+
+<!-- language: lang-js -->
+
+<!-- language: lang-css -->
+
+${indent4(mainCSS)}
+
+<!-- language: lang-html -->
+
+${indent4(mainHTML)}
+    <script type="module">
+${indent4(mainJS)}
+    </script>
+
+<!-- end snippet -->
+`
+    : `
+<!-- begin snippet: js hide: false console: true babel: false -->
+
+<!-- language: lang-js -->
+
+${indent4(mainJS)}
+
+<!-- language: lang-css -->
+
+${indent4(mainCSS)}
+
+<!-- language: lang-html -->
+
+${indent4(mainHTML)}
+
+<!-- end snippet -->
+`;
+  const dialogElem = document.querySelector('.copy-dialog');
+  dialogElem.style.display = '';
+  const copyAreaElem = dialogElem.querySelector('.copy-area');
+  copyAreaElem.textContent = text;
+  const linkElem = dialogElem.querySelector('a');
+  const tags = lessonEditorSettings.tags.filter(f => !f.endsWith('.org')).join(' ');
+  linkElem.href = `https://stackoverflow.com/questions/ask?&tags=javascript ${tags}`;
+}
+
+document.querySelectorAll('.dialog').forEach(dialogElem => {
+  dialogElem.addEventListener('click', function(e) {
+    if (e.target === this) {
+      this.style.display = 'none';
+    }
+  });
+  dialogElem.addEventListener('keydown', function(e) {
+    console.log(e.keyCode);
+    if (e.keyCode === 27) {
+      this.style.display = 'none';
+    }
+  })
+});
+const exportDialogElem = document.querySelector('.export');
+
+function openExport() {
+  exportDialogElem.style.display = '';
+  exportDialogElem.firstElementChild.focus();
+}
+
+function closeExport(fn) {
+  return () => {
+    exportDialogElem.style.display = 'none';
+    fn();
+  };
+}
+document.querySelector('.button-export').addEventListener('click', openExport);
+
 function selectFile(info, ndx, fileDivs) {
   if (info.editors.length <= 1) {
     return;
@@ -643,8 +797,10 @@ function setupEditor() {
   g.iframe = document.querySelector('.result>iframe');
   g.other = document.querySelector('.panes .other');
 
-  document.querySelector('.button-codepen').addEventListener('click', openInCodepen);
-  document.querySelector('.button-jsfiddle').addEventListener('click', openInJSFiddle);
+  document.querySelector('.button-codepen').addEventListener('click', closeExport(openInCodepen));
+  document.querySelector('.button-jsfiddle').addEventListener('click', closeExport(openInJSFiddle));
+  document.querySelector('.button-jsgist').addEventListener('click', closeExport(openInJSGist));
+  document.querySelector('.button-stackoverflow').addEventListener('click', closeExport(openInStackOverflow));
 
   g.result = document.querySelector('.panes .result');
   g.resultButton = document.querySelector('.button-result');
@@ -655,7 +811,7 @@ function setupEditor() {
   g.result.style.display = 'none';
   toggleResultPane();
 
-  if (window.innerWidth > 1200) {
+  if (window.innerWidth >= 1000) {
     toggleSourcePane(htmlParts.js.button);
   }
 
@@ -801,7 +957,9 @@ function getActualLineNumberAndMoveTo(url, lineNo, colNo) {
           column: colNo,
         });
         editor.revealLineInCenterIfOutsideViewport(actualLineNo);
-        editor.focus();
+        if (g.visible) {
+          editor.focus();
+        }
       }
     }
   }
