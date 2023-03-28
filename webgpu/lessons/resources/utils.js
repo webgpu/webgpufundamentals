4d758fb846af232cd84d1ccc73430a6819d7f5d7
greggman <github@greggman.com>
2023-03-28 01:23:38 +0000

    Deploying to gh-pages from  @ b65aec0b88760e998b19da12d9514a555bd27953 🚀

--- webgpu/lessons/resources/utils.js
+++ webgpu/lessons/resources/utils.js
@@ -5,3 +5,29 @@ const kebabRE = /-([a-z])/g;
 export function kebabCaseToCamelCase(s) {
   return s.replace(kebabRE, (_, m1) => m1.toUpperCase());
 }
+
+export function convertHexToBytes(text) {
+  const array = [];
+  for (let i = 0; i < text.length; i += 2) {
+    const tmpHex = text.substring(i, i + 2);
+    array.push(parseInt(tmpHex, 16));
+  }
+  return array;
+}
+
+export function convertBytesToHex(byteArray) {
+  let hex = '';
+  const il = byteArray.length;
+  for (let i = 0; i < il; i++) {
+    if (byteArray[i] < 0) {
+      byteArray[i] = byteArray[i] + 256;
+    }
+    let tmpHex = byteArray[i].toString(16);
+    // add leading zero
+    if (tmpHex.length === 1) {
+      tmpHex = '0' + tmpHex;
+    }
+    hex += tmpHex;
+  }
+  return hex;
+}
