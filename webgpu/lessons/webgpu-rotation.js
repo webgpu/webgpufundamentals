170701d3c4a5a9d3bf7991c0208eab6da760851b
greggman <github@greggman.com>
2023-04-09 07:59:03 +0000

    Deploying to gh-pages from  @ 5e0e1339c74d9c51ab9a373a51f5e2f23a170642 🚀

--- webgpu/lessons/webgpu-rotation.js
+++ webgpu/lessons/webgpu-rotation.js
@@ -13,4 +13,22 @@ renderDiagrams({
     const circle = new UnitCircle();
     div.appendChild(circle.domElement);
   },
+  'static-circle-30': (elem) => {
+    const div = el('div');
+    elem.appendChild(div);
+    const circle = new UnitCircle({
+      angle: 30 * Math.PI / 180,
+      frozen: true,
+    });
+    div.appendChild(circle.domElement);
+  },
+  'static-circle-60': (elem) => {
+    const div = el('div');
+    elem.appendChild(div);
+    const circle = new UnitCircle({
+      angle: 60 * Math.PI / 180,
+      frozen: true,
+    });
+    div.appendChild(circle.domElement);
+  },
 });
