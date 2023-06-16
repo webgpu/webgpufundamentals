e0a716292b8ce30746cc00e1f791c9f5a7cdd460
greggman <github@greggman.com>
2023-06-16 21:13:05 +0000

    Deploying to gh-pages from  @ b051a95a669984eb6f4a49ef224121e1b940f692 🚀

--- 3rdparty/webgpu-utils.module.js
+++ 3rdparty/webgpu-utils.module.js
@@ -3490,7 +3490,7 @@ function generateMipmap(device, texture) {
             @vertex fn vs(
                @builtin(vertex_index) vertexIndex : u32
             ) -> VSOutput {
-               var pos = array<vec2f, 3>(
+               let pos = array(
                   vec2f(-1.0, -1.0),
                   vec2f(-1.0,  3.0),
                   vec2f( 3.0, -1.0),
