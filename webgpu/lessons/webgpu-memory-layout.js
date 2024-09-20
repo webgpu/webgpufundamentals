80c436a8c649f68c980b5d584bf665f0d23bad48
greggman <github@greggman.com>
2024-09-20 03:07:08 +0000

    Deploying to gh-pages from  @ cecee473859daf547adee498e337008d1a49e19a 🚀

--- webgpu/lessons/webgpu-memory-layout.js
+++ webgpu/lessons/webgpu-memory-layout.js
@@ -11,6 +11,9 @@ import {
 import {
   makeTable,
 } from './resources/elem.js';
+import {
+  makeElem
+} from './resources/jsonml.js';
 import typeInfo from './resources/wgsl-data-types.js';
 
 renderDiagrams({
@@ -92,5 +95,162 @@ renderDiagrams({
         .filter(([name]) => name.length === 3 || name.includes('<'))) {
       addRow([name, size, align]);
     }
+  },
+
+  typedArrays(elem) {
+    const viewCtors = [
+      Int8Array,
+      Uint8Array,
+      Int16Array,
+      Uint16Array,
+      Int32Array,
+      Uint32Array,
+      Float32Array,
+      Float64Array,
+      BigInt64Array,
+      BigUint64Array,
+    ];
+
+    const numBytes = 16;
+
+    const range = (num, fn) => new Array(num).fill(0).map((_, i) => fn(i));
+
+    const arrayBuffer = new ArrayBuffer(numBytes);
+    const views = viewCtors.map(Ctor => new Ctor(arrayBuffer));
+    const f32 = new Float32Array(arrayBuffer);
+    f32.set([123, -456, 7.89, -0.123]);
+
+    const updateFns = [];
+    function updateAll() {
+      for (const fn of updateFns) {
+        fn();
+      }
+    }
+
+    let showAsHex = false;
+
+    function format(view, v) {
+      if (showAsHex && view.constructor.name.includes('nt')) {
+        return v.toString(16);//.padStart(view.BYTES_PER_ELEMENT * 2, '0');
+      } else {
+        return v.toString();
+      }
+    }
+
+    function parseBigInt16(v) {
+      v = v.trim();
+      const [start, sign] = v[0] === '-'
+        ? [1, -1]
+        : [0,  1];
+      let result = BigInt(0);
+      for (let i = start; i < v.length; ++i) {
+        const c = v[i].toLowerCase().charCodeAt(0);
+        let digit;
+        if (c >= 0x30 && c <= 0x39) {
+          digit = c - 0x30;
+        } else if (c >= 0x61 && c <= 0x66) {
+          digit = c - 0x61 + 10;
+        } else {
+          throw new Error('not hex');
+        }
+        result = result * BigInt(0x10) + BigInt(digit);
+      }
+      return result * BigInt(sign);
+    }
+
+    function parseBigInt(v) {
+      return showAsHex ? parseBigInt16(v) : BigInt(v);
+    }
+
+    const intRE = /^-?\d+$/;
+    const hexRE = /^-?[0-9a-f]+$/i;
+    function parseIntNumber(v) {
+      if (showAsHex) {
+        if (!hexRE.test(v)) {
+          throw new Error('not hex');
+        }
+      } else {
+        if (!intRE.test(v)) {
+          throw new Error('not int');
+        }
+      }
+      return parseInt(v, showAsHex ? 16 : 10);
+    }
+
+    function parseFloatNumber(v) {
+      v = Number(v);
+      if (isNaN(v)) {
+        throw Error('NaN');
+      }
+      return v;
+    }
+
+    elem.appendChild(makeElem([
+      'table',
+      [
+        'thead',
+        ['th', 'arrayBuffer'],
+        ...range(numBytes, i =>  ['th', `${i}`]),
+      ],
+      [
+        'tbody',
+        ...views.map((view) => {
+          const numCellsPerElem = view.BYTES_PER_ELEMENT;
+          const numElems = numBytes / numCellsPerElem;
+          const parse = view instanceof BigInt64Array
+            ? parseBigInt
+            : view instanceof BigUint64Array
+            ? parseBigInt
+            : view.constructor.name.includes('Float')
+            ? parseFloatNumber
+            : parseIntNumber;
+          return [
+            'tr',
+            ['td', `as${view.constructor.name.substring(0, view.constructor.name.length - 5)}` ],
+            ...range(numElems, i => {
+              const input = makeElem([
+                'input', {type: 'text', value: 123, onInput: function() {
+                  let err = false;
+                  try {
+                    view[i] = parse(this.value);
+                  } catch (error) {
+                    //console.log('here', error.message);
+                    err = true;
+                  }
+                  input.classList.toggle('error', err);
+                  updateAll();
+                },
+                },
+              ]);
+
+              updateFns.push(() => {
+                if (document.activeElement !== input) {
+                  input.value = format(view, view[i]);
+                  input.classList.remove('error');
+                }
+              });
+
+              return ['td', {colSpan: numCellsPerElem},
+                input,
+              ];
+            }),
+          ];
+        }),
+      ],
+    ]));
+
+    updateAll();
+
+    elem.appendChild(makeElem(['label',
+      elem.dataset.caption,
+      [
+        'input', { type: 'checkbox', onChange: function() {
+          showAsHex = this.checked;
+          updateAll();
+        },
+        },
+      ],
+    ]));
+
   },
 });
